f6bc25cd7efbe8fafbe51574eca36b8d7a54bcbf
NaderAzzabi <azzabinader773@gmail.com>
2023-05-08 15:11:01 +0100

    project1

--- Untitled-3.js
+++
@@ -1,7 +0,0 @@
-function markVisited(country) {
-    var li = document.getElementById(country);
-    var img = li.getElementsByTagName('img')[0];
-    img.setAttribute('src', 'visited.png');
-    img.setAttribute('alt', 'Visited');
-    li.innerHTML += ' Visited';
-  }
